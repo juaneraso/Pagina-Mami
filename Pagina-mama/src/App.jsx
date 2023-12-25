08c349fbf6a185b25ba32e550c3b36aa44483c54
Juan <juangui9508@hotmail.com>
2023-12-25 13:19:51 -0500

    first commit

---
+++ Pagina-mama/src/App.jsx
@@ -0,0 +1,14 @@
+import FelizNavidad from "./components/Feliznavidad/FelizNavidad"
+
+function App() {
+  const imagen1URL = 'https://firebasestorage.googleapis.com/v0/b/fir-autenticacion-95e3f.appspot.com/o/FotoconMami.jpeg?alt=media&token=e9155552-3076-433c-b977-9158382c258c';
+  const imagen2URL = 'https://firebasestorage.googleapis.com/v0/b/fir-autenticacion-95e3f.appspot.com/o/Foto%20Santafe.jpeg?alt=media&token=9b36606f-3e4a-438e-a0ba-7060af7fd9e4';
+
+  return (
+    <>
+     <FelizNavidad imagen1={imagen1URL} imagen2={imagen2URL}/>
+    </>
+  )
+}
+
+export default App
---
+++ Pagina-mama/src/components/FelizNavidad/FelizNavidad.jsx
@@ -0,0 +1,21 @@
+
+import style from "../FelizNavidad/FelizNavidad.module.css"
+const FelizNavidad = ({ imagen1, imagen2 }) => {
+
+
+ return (
+    <div className={style.container}>
+    <h1> 🎄 Feliz Navidad mami  🎅   </h1>
+   
+    <img src={imagen1} alt="Imagen 1" />
+    <img src={imagen2} alt="Imagen 2" />
+    <p> Te quiero mucho ❤️</p>
+  </div>
+
+ );
+
+
+
+};
+
+export default FelizNavidad;

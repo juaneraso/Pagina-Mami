1debc32cf49735a849ded81f15ddb7ff147d0ba9
Juan <juangui9508@hotmail.com>
2023-12-25 13:23:06 -0500

    second commit

--- Pagina-mama/src/components/FelizNavidad/FelizNavidad.jsx
+++ Pagina-mama/src/components/FelizNavidad/FelizNavidad.jsx
@@ -5,8 +5,7 @@ const FelizNavidad = ({ imagen1, imagen2 }) => {
 
  return (
     <div className={style.container}>
-    <h1> 🎄 Feliz Navidad mami  🎅   </h1>
-   
+    <h1> 🎄 Feliz Navidad mami  🎅   </h1>   
     <img src={imagen1} alt="Imagen 1" />
     <img src={imagen2} alt="Imagen 2" />
     <p> Te quiero mucho ❤️</p>
